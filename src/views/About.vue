c1dfb877ae077bfe3cc33efacbc15af516a78778
Murilo Boareto Delefrate <murilobd@gmail.com>
2020-06-18 13:56:39 -0300

    - initial commit with router and i18n

---
+++ src/views/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>{{ $t('title_about_page') }}</h1>
+    <pre>
+      Current locale is: {{ currentLocale }}
+    </pre>
+    <div>
+      <select name="locale" id="locale" v-model="currentLocale">
+        <option value="en">English</option>
+        <option value="fr">French</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+import i18n from "@/i18n";
+export default {
+  computed: {
+    currentLocale: {
+      get() {
+        return i18n.locale;
+      },
+      set(_newLocale) {
+        i18n.locale = _newLocale;
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div>
+      {{ $t('my_name') }}
+    </div>
+    <HelloWorld :msg="$t('message')"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
